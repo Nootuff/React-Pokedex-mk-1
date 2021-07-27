c76d2a94a19ba0f4688ba25b9be40f56fb03667b
Adam <adamwalkerlondon@gmail.com>
2021-07-27 18:54:04 +0100

    Styling finished.

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,10 +4,17 @@ import '../stylesheets/Pokedex.css';
 
 class Pokedex extends React.Component {
     render() {
+        let title;
+        if(this.props.isWinner){
+            title = <h1 className="Pokedex-winner">Winning hand</h1>;
+        } else {
+            title = <h1 className="Pokedex-loser">Losing hand</h1>;
+        }
+
         return (
             <div className="Pokedex">
                 <p>Total cp: {this.props.cp}</p>
-                <p>{this.props.isWinner ? "Winner" : "Loser"}</p>
+                {title}
                 <div className="Pokedex-cards"> {/*This is generally how to do your class names in react, this format of class name. */}
                 {this.props.pokemon.map((p) => ( /* Loop over the array of objects in pokemon which is being passed in as a prop. */
 <PokemonCard id={p.id} name={p.name} type={p.type} base_experience={p.base_experience}/> //Render a pokemon card object. 
